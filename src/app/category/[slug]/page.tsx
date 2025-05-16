ec554367b3486c8168c7db5803b2743068929e64
HameezExE <hameezexe@gmail.com>
2025-05-16 11:09:38 +0530

    Minor fix

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -2,7 +2,11 @@ import { generateStaticParams } from "@/app/post/[slug]/page";
 import { getAllPosts, Post } from "@/lib/posts";
 import PostFilter from "@/components/PostFilter";
 
-export default async function Category({ params }: Promise<{ slug: string }>) {
+export default async function CategoryPost({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
   const { slug } = await params;
   const posts = await getAllPosts();
   return <PostFilter posts={posts} category={slug} />;
