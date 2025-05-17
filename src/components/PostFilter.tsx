ffe42c2e4d1b9e5f102f8ccf88c2aff03d8016fa
HameezExE <hameezexe@gmail.com>
2025-05-18 01:13:51 +0530

    Fixed searchbar style

--- src/components/PostFilter.tsx
+++ src/components/PostFilter.tsx
@@ -28,14 +28,31 @@ export default function PostFilter({ posts, category }) {
 
   return (
     <>
-      <section>
+      <section className="min-h-[300px] bg-primary mt-10 rounded-xl flex justify-center flex-col items-center">
+        <h1></h1>
         <form>
-          <input
-            type="text"
-            value={term}
-            onChange={(e) => setTerm(e.target.value)}
-            className="bg-green-200"
-          />
+          <div className="relative">
+            <input
+              type="text"
+              value={term}
+              onChange={(e) => setTerm(e.target.value)}
+              className="bg-gray-100 rounded-md h-9 pl-9 pr-4 w-full"
+              placeholder="Search for blog posts..."
+              autoFocus
+            />
+            <div className="absolute inset-y-0 left-0 pl-2 flex items-center pointer-events-none">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                className="h-[18px] w-[18px] text-gray-500"
+              >
+                <path
+                  fill="currentColor"
+                  d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"
+                />
+              </svg>
+            </div>
+          </div>
         </form>
         <div>
           {categories.map((category) => (
